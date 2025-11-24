af6f55fb728e808029496f542cdf962643b2c7be
MIN <mhroh01@naver.com>
2025-11-24 20:49:17 +0900

    feat : fid on FM model

--- flow_matching/inference.py
+++ flow_matching/inference.py
@@ -1,9 +1,17 @@
-
-
+import data.get_data 
+import data.dataloader 
+from .flow_matching_model import *
+from .utils import *
 import os
 import torch
+import torch.nn as nn
+import argparse
+import numpy as np
+import wandb
+
 from tqdm import tqdm
-from torchvision.utils import save_image
+import torchvision
+from torchvision.utils import make_grid, save_image
 
 
 @torch.no_grad()
@@ -18,6 +26,8 @@ def show_prediction_fm_fid(
 ):
     model.eval()
     os.makedirs(out_dir, exist_ok=True)
+    out_dir = os.path.join(out_dir,f"step_{inference_step}")
+    
     real_dir = os.path.join(out_dir, "real")
     gen_dir  = os.path.join(out_dir, "gen")
     os.makedirs(real_dir, exist_ok=True)
@@ -57,3 +67,30 @@ def show_prediction_fm_fid(
 
     print(f"저장 완료: {real_dir}, {gen_dir}")
     return real_dir, gen_dir
+
+
+if __name__ == "__main__":
+    parser =argparse.ArgumentParser()
+    parser.add_argument("--inference_step",type=int, default=20)
+    args = parser.parse_args()
+
+    inference_step = args.inference_step
+    device = "cuda"
+    
+    valset = data.dataloader.CustomDataset(test=True)
+    valloader = torch.utils.data.DataLoader(valset,batch_size=16,num_workers=4,shuffle=False)
+
+    model = FlowMatchingUnet().to(device)
+    fm_scheduler = FlowMatchingScheduler(model_type="optimal transport").to(device)
+    
+    model.load_state_dict(torch.load("checkpoints/FlowMatching.pth", map_location=device))
+
+    real_dir, gen_dir = show_prediction_fm_fid(
+        valloader=valloader,
+        fm_scheduler=fm_scheduler,
+        model=model,
+        device=device,
+        inference_step=inference_step,
+        out_dir="checkpoints/fm/val_samples"
+    )
+
